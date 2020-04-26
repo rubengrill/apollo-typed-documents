529073b0d13a2ff18ce2b6554079b810a003b8f1
Ruben Grill <ruben@purelabs.io>
2020-04-26 16:03:31 +0200

    Implement cra-ts example

--- examples/cra-ts/src/index.tsx
+++ examples/cra-ts/src/index.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { MockedProvider } from "@apollo/react-testing";
+import React from "react";
+import ReactDOM from "react-dom";
+
+import apolloMock from "./apolloMock";
+import App from "./App";
+import authorsQuery from "./authors.graphql";
+import createAuthorMutation from "./createAuthor.graphql";
+
+const mocks = [
+  apolloMock(authorsQuery, {}, { authors: [] }),
+  apolloMock(
+    createAuthorMutation,
+    { input: { name: "Foo", books: [{ title: "Bar" }] } },
+    { createAuthor: { name: "Foo", books: [{ title: "Bar" }] } }
+  ),
+  apolloMock(
+    authorsQuery,
+    {},
+    { authors: [{ name: "Foo", books: [{ title: "Bar" }] }] }
+  ),
+];
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MockedProvider mocks={mocks}>
+      <App />
+    </MockedProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
